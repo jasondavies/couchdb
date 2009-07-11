4a11fa049328325a50eb17d67f0e0df3ff7e0038
Jason Davies <jason@jdd.local>
2009-07-11 21:22:15 +0100

    Basic demo of OAuth.

--- src/couchdb/couch_httpd_oauth.erl
+++ src/couchdb/couch_httpd_oauth.erl
@@ -13,14 +13,16 @@
 -module(couch_httpd_oauth).
 -include("couch_db.hrl").
 
--export([handle_oauth_request/1]).
+-export([handle_oauth_req/1]).
 
 -import(couch_httpd, [header_value/2, send_json/4, send_method_not_allowed/2]).
 -import(erlang, [integer_to_list/2, list_to_integer/2]).
--import(proplists, [get_value/2]).
+-import(proplists, [get_value/2, get_value/3]).
 
-handle_oauth_request(Request) ->
-    null.
+handle_oauth_req(#httpd{method='GET', path_parts=[_OAuth, <<"request_token">>], mochi_req=MochiReq}=Req) ->
+    serve_oauth_request_token(MochiReq);
+handle_oauth_req(#httpd{method='GET', path_parts=[_OAuth, <<"access_token">>], mochi_req=MochiReq}=Req) ->
+    serve_oauth_access_token(MochiReq).
 
 serve_oauth_request_token(Request) ->
     case Request:get(method) of
@@ -30,7 +32,7 @@ serve_oauth_request_token(Request) ->
                     true ->
                         ok(Request, <<"oauth_token=requestkey&oauth_token_secret=requestsecret">>);
                     false ->
-                        bad(Request, "invalid signature value.")
+                        bad(Request, "Invalid signature value.")
                 end
             end);
         _ ->
@@ -47,10 +49,10 @@ serve_oauth_access_token(Request) ->
                             true ->
                                 ok(Request, <<"oauth_token=accesskey&oauth_token_secret=accesssecret">>);
                             false ->
-                                bad(Request, "invalid signature value.")
+                                bad(Request, "Invalid signature value.")
                         end;
                     _ ->
-                        bad(Request, "invalid oauth token.")
+                        bad(Request, "Invalid OAuth token.")
                 end
             end);
         _ ->
@@ -68,10 +70,10 @@ serve_echo(Request) ->
                                 EchoParams = lists:filter(fun({K, _}) -> not lists:prefix("oauth_", K) end, Params),
                                 ok(Request, oauth_uri:params_to_string(EchoParams));
                             false ->
-                                bad(Request, "invalid signature value.")
+                                bad(Request, "Invalid signature value.")
                         end;
                     _ ->
-                        bad(Request, "invalid oauth token")
+                        bad(Request, "Invalid OAuth token.")
                 end
             end);
         _ ->
@@ -80,20 +82,20 @@ serve_echo(Request) ->
  
 serve_oauth(Request, Fun) ->
     Params = Request:parse_qs(),
-    case get_value("oauth_version", Params) of
+    case get_value("oauth_version", Params, "1.0") of
         "1.0" ->
             ConsumerKey = get_value("oauth_consumer_key", Params),
             SigMethod = get_value("oauth_signature_method", Params),
             case consumer_lookup(ConsumerKey, SigMethod) of
                 none ->
-                    bad(Request, "invalid consumer (key or signature method).");
+                    bad(Request, "Invalid consumer (key or signature method).");
                 Consumer ->
                     Signature = proplists:get_value("oauth_signature", Params),
                     URL = string:concat("http://0.0.0.0:8000", Request:get(path)),
                     Fun(URL, proplists:delete("oauth_signature", Params), Consumer, Signature)
             end;
         _ ->
-            bad(Request, "invalid oauth version.")
+            bad(Request, "Invalid OAuth version.")
     end.
 
 consumer_lookup("key", "PLAINTEXT") ->
