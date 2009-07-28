3ad308a7384f6b0304e0d781e673c169b56c6b4f
Jason Davies <jason@jdd.local>
2009-07-28 12:06:27 +0100

    Remove more -imports as per Jan's recommendation.

--- src/couchdb/couch_httpd_oauth.erl
+++ src/couchdb/couch_httpd_oauth.erl
@@ -15,11 +15,6 @@
 
 -export([oauth_authentication_handler/1, handle_oauth_req/1, consumer_lookup/2]).
 
--import(couch_httpd, [header_value/2, send_error/4, send_json/4,
-        send_method_not_allowed/2]).
--import(erlang, [integer_to_list/2, list_to_integer/2]).
--import(proplists, [get_value/2, get_value/3]).
-
 % OAuth auth handler using per-node user db
 oauth_authentication_handler(#httpd{method=Method}=Req) ->
     serve_oauth(Req, fun(URL, Params, Consumer, Signature) ->
@@ -75,14 +70,14 @@ handle_oauth_req(#httpd{path_parts=[_OAuth, <<"access_token">>], method='GET'}=R
                         invalid_signature(Req)
                 end;
             _ ->
-                send_error(Req, 400, <<"invalid_token">>, <<"Invalid OAuth token.">>)
+                couch_httpd:send_error(Req, 400, <<"invalid_token">>, <<"Invalid OAuth token.">>)
         end
     end, false);
 handle_oauth_req(#httpd{path_parts=[_OAuth, <<"access_token">>]}=Req) ->
-    send_method_not_allowed(Req, "GET").
+    couch_httpd:send_method_not_allowed(Req, "GET").
 
 invalid_signature(Req) ->
-    send_error(Req, 400, <<"invalid_signature">>, <<"Invalid signature value.">>).
+    couch_httpd:send_error(Req, 400, <<"invalid_signature">>, <<"Invalid signature value.">>).
 
 % This needs to be protected i.e. force user to login using HTTP Basic Auth or form-based login.
 serve_oauth_authorize(#httpd{method=Method}=Req) ->
@@ -113,7 +108,7 @@ serve_oauth_authorize(#httpd{method=Method}=Req) ->
                 end
             end, false);
         _ ->
-            send_method_not_allowed(Req, "GET,POST")
+            couch_httpd:send_method_not_allowed(Req, "GET,POST")
     end.
 
 serve_oauth(#httpd{mochi_req=MochiReq, req_body=ReqBody, method=Method}=Req, Fun, FailSilently) ->
@@ -149,19 +144,19 @@ serve_oauth(#httpd{mochi_req=MochiReq, req_body=ReqBody, method=Method}=Req, Fun
             oauth_uri:params_from_header_string(HeaderString)
     end,
     ?LOG_DEBUG("OAuth Params: ~p", [Params]),
-    case get_value("oauth_version", Params, "1.0") of
+    case proplists:get_value("oauth_version", Params, "1.0") of
         "1.0" ->
-            case get_value("oauth_consumer_key", Params, undefined) of
+            case proplists:get_value("oauth_consumer_key", Params, undefined) of
                 undefined ->
                     case FailSilently of
                         true -> Req;
-                        false -> send_error(Req, 400, <<"invalid_consumer">>, <<"Invalid consumer.">>)
+                        false -> couch_httpd:send_error(Req, 400, <<"invalid_consumer">>, <<"Invalid consumer.">>)
                     end;
                 ConsumerKey ->
-                    SigMethod = get_value("oauth_signature_method", Params),
+                    SigMethod = proplists:get_value("oauth_signature_method", Params),
                     case consumer_lookup(ConsumerKey, SigMethod) of
                         none ->
-                            send_error(Req, 400, <<"invalid_consumer">>, <<"Invalid consumer (key or signature method).">>);
+                            couch_httpd:send_error(Req, 400, <<"invalid_consumer">>, <<"Invalid consumer (key or signature method).">>);
                         Consumer ->
                             Signature = proplists:get_value("oauth_signature", Params),
                             URL = couch_httpd:absolute_uri(Req, MochiReq:get(path)),
@@ -171,7 +166,7 @@ serve_oauth(#httpd{mochi_req=MochiReq, req_body=ReqBody, method=Method}=Req, Fun
                     end
             end;
         _ ->
-            send_error(Req, 400, <<"invalid_oauth_version">>, <<"Invalid OAuth version.">>)
+            couch_httpd:send_error(Req, 400, <<"invalid_oauth_version">>, <<"Invalid OAuth version.">>)
     end.
 
 consumer_lookup(Key, MethodStr) ->
